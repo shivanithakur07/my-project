5b0d4b195014d7e6efe65e8327cb956e95a51a61
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-13 21:02:53 +0530

    Add files via upload

---
+++ React-Project/Experiment-7/Experiment-8/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import LibraryManagement from './components/LibraryManagement'
+
+function App() {
+  return (
+    <div className="App">
+      <LibraryManagement />
+    </div>
+  )
+}
+
+export default App
